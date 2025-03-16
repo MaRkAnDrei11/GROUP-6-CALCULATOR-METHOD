c827e7b5b83515edd9ce60b368d390ab2198472c
MaRkAnDrei11 <mpsanmiguel@paterostechnologicalcollege.edu.ph>
2025-03-16 11:44:32 +0800

    Update CALCULATOR.JAVA GROUP 6.java
    Signed-off-by: MaRkAnDrei11 <mpsanmiguel@paterostechnologicalcollege.edu.ph>

--- CALCULATOR.JAVA GROUP 6.java
+++ CALCULATOR.JAVA GROUP 6.java
@@ -1,3 +1,3 @@
-// Group 6: Square (Members: JARDELEZA, SAN MIGUEL, GABRILLO, SANTANDER, ORDINARIO, MUYRONG, ABOHAN, ANTONIO)
+// Group 6: Square (Members: JARDELEZA, SAN MIGUEL, GABRILLO, SANTANDER, HALLIG, ORDINARIO, MUYRONG, ABOHAN, ANTONIO)
 public static int square(int a) {
     return a * a;
